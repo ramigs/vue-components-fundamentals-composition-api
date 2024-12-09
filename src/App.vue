a7b6afd26ebd90efc98e94f2b6be11f343d3f577
ramigs <ramigs@gmail.com>
2024-12-09 17:08:01 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-
-const myArray = [1, 2, 3]
+import CoffeePlan from './components/CoffeePlan.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-    <h2 v-for="element in myArray">
-      {{ element }}
-    </h2>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="content">
+      <h1 class="title">Coffee Plans</h1>
+
+      <h2 class="subtitle">
+        We travel the world to source the very best single origin coffee for you
+      </h2>
+
+      <div class="plans">
+        <CoffeePlan name="The Single" />
+        <CoffeePlan name="The Curious" />
+        <CoffeePlan name="The Addict" />
+        <CoffeePlan name="The Hacker" />
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>()
+</script>
+
+<template>
+  <div class="plan">
+    <div class="description">
+      <span class="title"> {{ name }} </span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
